2442a183bb79fd70ffdd4f7de7ea88dd8cb7ac4b
ayşe günel <gunel.ayse7@gmail.com>
2023-07-27 12:29:15 +0300

    publisher commit

---
+++ RabbitMqOperations.Publisher/MassTransitConsoleHostedService.cs
@@ -0,0 +1,50 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMqOperations.Common.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqOperations.Publisher
+{
+    public class MassTransitConsoleHostedService :
+        IHostedService
+    {
+        readonly IBusControl _bus;
+        readonly ILogger _logger;
+
+        public MassTransitConsoleHostedService(IBusControl bus, ILoggerFactory loggerFactory)
+        {
+            _bus = bus;
+            _logger = loggerFactory.CreateLogger<MassTransitConsoleHostedService>();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting bus");
+            await _bus.StartAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+
+            var i = 0;
+
+            while (true)
+            {
+                i++;
+                await _bus.Publish(new UrlMessage() { Url = "https://twitter.com/home" + i.ToString() }, cancellationToken)
+                    .ConfigureAwait(false);
+
+                await Task.Delay(1000);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping bus");
+            return _bus.StopAsync(cancellationToken);
+        }
+    }
+}
---
+++ RabbitMqOperations.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMqOperations.Common;
+using RabbitMqOperations.Publisher;
+
+var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                        config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<RabbitMqConfig>(hostContext.Configuration.GetSection("AppConfig"));
+
+                    services.AddMassTransit(cfg =>
+                    {
+                        cfg.SetKebabCaseEndpointNameFormatter();
+
+                        cfg.AddBus(context => RabbitMassTransitConfiguration.ConfigureBus(context));
+                    });
+
+                    services.AddHostedService<MassTransitConsoleHostedService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+await builder.RunConsoleAsync();
